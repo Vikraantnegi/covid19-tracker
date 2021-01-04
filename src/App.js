45097ff73cccd1cda19cee1a81b649539ade2c7d
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-01-04 15:01:36 +0530

    ADD: Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
+import './styles/HelperStyles.css';
+import {FormControl, MenuItem, Select} from '@material-ui/core'
 
 function App() {
   return (
     <div className="App">
-      <h1>Let's build covid19 tracker boiss</h1>
+      <div className="covid-header flexRow flexBetween flexAlignCenter mb-20">
+        <h1>Covid-19 Tracker</h1>
+        <FormControl className="country-dropdown">
+          <Select variant='outlined' value='Select Location'>
+            <MenuItem value='Worldwide'>WorldWide</MenuItem>
+            <MenuItem value='India'>India</MenuItem>
+            <MenuItem value='US'>US</MenuItem>
+            <MenuItem value='UK'>UK</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
