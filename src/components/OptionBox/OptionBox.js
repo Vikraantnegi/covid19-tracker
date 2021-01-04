6d1bf21d071fce2ce76680cf96bf014decc0aa11
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-01-04 22:07:25 +0530

    ADD: FeatureBoxes

---
+++ src/components/OptionBox/OptionBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './OptionBox.css';
+import '../../styles/HelperStyles.css';
+import {Card, CardContent, Typography} from '@material-ui/core';
+
+const OptionBox = (props) => {
+    return(
+        <Card></Card>
+    );
+}
+
+export default OptionBox;
