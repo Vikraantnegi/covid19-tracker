298cf73846bbf7010d2b853b660f95eaa96eba2c
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-01-05 02:14:30 +0530

    ADD: Map Component

---
+++ src/components/Map/Map.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Map.css';
+import '../../styles/HelperStyles.css';
+
+function Map() {
+    return (
+        <div className="map">
+            Hey, this is a map.
+        </div>
+    )
+}
+
+export default Map
