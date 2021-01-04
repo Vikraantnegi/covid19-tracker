5d3eff7bbf59923eab91a64f7268d8538cb56450
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-01-04 18:58:54 +0530

    UPDATE : Functional Dropdown

--- src/App.js
+++ src/App.js
@@ -4,18 +4,17 @@ import './styles/HelperStyles.css';
 import {FormControl, MenuItem, Select} from '@material-ui/core'
 
 function App() {
-  const [countries, setcountries] = useState(['Worldwide', 'US', 'UK', 'India', 'Brazil', 'Argentina', 'Israel']);
+  const [countries, setcountries] = useState([]);
+  const [country, setcountry] = useState('WoW');
 
   useEffect(() => {
     const getCountryData = async () => {
       await fetch('https://disease.sh/v3/covid-19/countries')
-        .then(response => {
-          response.json()
-        })
+        .then(response => response.json())
         .then(data => {
           const countries = data.map(country => ({
             name: country.country,
-            code: country.countryInfo.iso2,
+            code: country.countryInfo.iso3,
             // flag: country.countryInfo.flag,
             // totalcases: country.cases,
             // newcases: country.todayCases,
@@ -26,18 +25,23 @@ function App() {
             // active: country.active,
             // critical: country.critical,
           }))
+          setcountries(countries);
         })
+        .catch(error => console.log(error));
+    };
+    getCountryData();
 
-    }
-  }, [countries])
+  }, []);
+
   return (
     <div className="App">
       <div className="covid-header flexRow flexBetween flexAlignCenter mb-20">
         <h1>Covid-19 Tracker</h1>
         <FormControl className="country-dropdown">
-          <Select variant='outlined' value='Select Location'>
+          <Select variant='outlined' defaultValue={country} onChange={(e) => setcountry(e.target.value)}>
+            <MenuItem value="WoW">WorldWide</MenuItem>
             {countries.map(country => (
-              <MenuItem value={country}>{country}</MenuItem>
+              <MenuItem value={country.code}>{country.name}</MenuItem>
             ))}
           </Select>
         </FormControl>
