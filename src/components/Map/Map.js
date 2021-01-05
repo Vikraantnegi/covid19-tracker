62659da08a32b33bf72e2867c96b41f37cc0811c
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-01-05 22:29:51 +0530

    UPDATE: Map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,18 +1,60 @@
-import React from 'react';
-import './Map.css';
-import '../../styles/HelperStyles.css';
-import { MapContainer, TileLayer } from 'react-leaflet';
+  
+import React from "react";
+import { MapContainer, TileLayer, Circle, Popup, useMap } from "react-leaflet";
+import "./Map.css";
 
-function CovidMap(props) {
+const casesTypeColors = {
+    cases: {
+      hex: "#CC1034",
+      multiplier: 800,
+    },
+    recovered: {
+      hex: "#7dd71d",
+      multiplier: 1200,
+    },
+    deaths: {
+      hex: "#fb4443",
+      multiplier: 2000,
+    },
+  };
+  
+  const showMapData = (data, casesType = "cases") =>
+    data.map((country) => (
+      <Circle
+        center={[country.countryInfo.lat, country.countryInfo.long]}
+        color={casesTypeColors[casesType].hex}
+        fillColor={casesTypeColors[casesType].hex}
+        fillOpacity={0.4}
+        radius={
+          Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
+        }
+      >
+        <Popup>
+          Hi I am a Popup!
+        </Popup>
+      </Circle>
+    ));
+
+
+function Map({ countries, casesType, center, zoom }) {
+    const ChangeView = ({ center, zoom }) => {
+        const map = useMap();
+        map.setView(center, zoom);
+        return null;
+    }
+
     return (
         <div className="map">
-            <MapContainer center={props.center} zoom={props.zoom}>
+            <MapContainer center={center} zoom={zoom}>
+                <ChangeView center={center} zoom={zoom} /> 
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 />
+                {showMapData(countries, casesType)}
             </MapContainer>
         </div>
-    )
+    );
 }
 
-export default CovidMap;
+export default Map;
