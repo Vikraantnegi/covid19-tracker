f5d2cc6d746c8144f44da940132cbea50bed7b93
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-01-05 18:39:02 +0530

    ADD: Covid-Map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './Map.css';
 import '../../styles/HelperStyles.css';
+import {Map as Map1, TileLayer} from 'react-leaflet';
 
 function Map() {
     return (
-        <div className="map">
-            Hey, this is a map.
+        <div className="covid-map">
+            <Map1>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution= '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+            </Map1>
         </div>
     )
 }
