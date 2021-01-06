f4a837388ab64900883f994ef8fa20596cd647b1
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-01-07 04:58:50 +0530

    UPDATE: App

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ function App() {
   const [mapCenter, setMapCenter] = useState([ 34.80746, -40.4796 ]);
   const [mapZoom, setMapZoom] = useState(2);
   const [mapCountries, setMapCountries] = useState([]);
-  const [caseType, setcaseType] = useState('cases');
 
   const sortData = (data) => {
     const sortedData = [...data];
@@ -27,7 +26,7 @@ function App() {
   }
 
   const numberFormat = (data) => 
-    data ? `${numeral(data).format("0.0a")}` : "0";
+    data ? `${numeral(data).format("0,0")}` : "0";
 
     const titleCase = (str) => {
       str = str.toLowerCase()
@@ -80,7 +79,7 @@ function App() {
           ) : (
             setMapCenter([ data.countryInfo.lat, data.countryInfo.long ])
           )}
-          setMapZoom(3);
+          setMapZoom(4);
           setcountry(countryCode);
           setcountryInfo(data);
         })
@@ -93,76 +92,68 @@ function App() {
         <h1 className="covid-header">Covid-19 Tracker</h1>
         <FormControl className="country-dropdown">
           <Select variant='outlined' defaultValue={country} onChange={(e) => DropdownChange(e)}>
-            <MenuItem value="WoW">WorldWide</MenuItem>
+            <MenuItem value="WoW"><strong>WorldWide</strong></MenuItem>
             {countries.map(country => (
-              <MenuItem key={country.code} value={country.code}>{country.name}</MenuItem>
+              <MenuItem key={country.code} value={country.code}><strong>{country.name}</strong></MenuItem>
             ))}
           </Select>
         </FormControl>
       </div>
       <div className="covid-stats flexRow flexAlignCenter">
-        <div className="total-covid flexColumn">
-
-        </div> 
-        <div className="overview-covid flexColumn">
-          
-        </div> 
-        <div className="trends-covid flexColumn">
-          <div className="trends-head flexRow flexBetween flexAlignCenter mh-10">
-            <h3 className="fresh-cases" style={{fontWeight: 600}}>Trends</h3>
-            <FormControl className="trends-dropdown">
-                <Select variant='outlined' defaultValue={caseType} onChange={(e) => setcaseType(e.target.value)}>
-                    <MenuItem value='cases'><strong>Total Cases</strong></MenuItem>
-                    <MenuItem value='recovered'><strong>Recovered</strong></MenuItem>
-                    <MenuItem value='deaths'><strong>Total Deaths</strong></MenuItem>
-                </Select>
-            </FormControl>
+        <div className="total-covid grow flexColumn">
+          <h3 style={{marginTop: 20, marginBottom: 0}}>Total Confirmed Covid Cases</h3>
+          <h1 className="total-cases">{numberFormat(countryInfo.cases)}</h1>
+          <div className="flexColumn flexAlignCenter">
+            <div className="stat-box flexRow flexEnd">
+              <h4 style={{margin:0, color: '#666666'}} className="flexColumn flexCenter">+ {numberFormat(countryInfo.todayCases)} new cases</h4>
+            </div>
+            <FeatureBox 
+              casetype="Critical"  
+              freshCases='' 
+              totalCases={numberFormat(countryInfo.critical)} 
+              color="#de3700"
+            />
           </div>
-          <Graph countryCode={country} casesType={caseType} />
+        </div> 
+        <div className="overview-covid grow flexColumn">
+          <h3 style={{marginTop: 20, marginBottom: 20}}>Overview</h3>
+          <div className="flexColumn flexAlignCenter">
+            <FeatureBox 
+              casetype="Active" 
+              freshCases='' 
+              totalCases={numberFormat(countryInfo.active)} 
+              color="#f4c363"
+            />
+            <FeatureBox 
+              casetype="Recovered" 
+              freshCases={numberFormat(countryInfo.todayRecovered)} 
+              totalCases={numberFormat(countryInfo.recovered)} 
+              color="#60bb69"
+            />
+            <FeatureBox 
+              casetype="Fatal" 
+              freshCases={numberFormat(countryInfo.todayDeaths)} 
+              totalCases={numberFormat(countryInfo.deaths)} 
+              color="#767676"
+            />
+            <h5 className="covid-source w100">Source : https://disease.sh/</h5>
+          </div>
+        </div> 
+        <div className="trends-covid grow flexColumn">
+          <Graph countryCode={country} />
         </div> 
       </div>
       <div className="covid-app flexRow flexAround">
         <div className="stats-left">
-          {/* <div className="covid-total flexRow flexEnd">
-            <h1 className="total-cases mr-20">{numberFormat(countryInfo.cases)}</h1>
-            <div className="flexColumn flexAlignCenter flexCenter mr-20">
-              <h4 style={{margin:0}} className="critical-cases flexColumn flexCenter">{numberFormat(countryInfo.critical)}</h4>
-              <h4 style={{margin:0}} className="critical">Critical</h4>
-            </div>
-          </div> */}
-          {/* <div className="covid-stats flexRow flexBetween flexAlignCenter">
-            <FeatureBox 
-              sr='active'
-              active={casesType === 'cases'}
-              onClick={(e) => setCasesType('cases')}
-              title="Total Active Cases" 
-              freshCases={numberFormat(countryInfo.todayCases)} 
-              totalCases={numberFormat(countryInfo.active)} 
-            />
-            <FeatureBox 
-              sr='recovered'
-              active={casesType === 'recovered'}
-              onClick={(e) => setCasesType('recovered')}
-              title="Total Recovered Cases" 
-              freshCases={numberFormat(countryInfo.todayRecovered)} 
-              totalCases={numberFormat(countryInfo.recovered)} 
-            />
-            <FeatureBox
-              sr='deaths'
-              active={casesType === 'deaths'}
-              onClick={(e) => setCasesType('deaths')}
-              title="Total Covid Deaths" 
-              freshCases={numberFormat(countryInfo.todayDeaths)} 
-              totalCases={numberFormat(countryInfo.deaths)} 
-            />
-          </div> */}
           <div className="covid-map">
             <Map 
-            // casesType={casesType} 
-            countries={mapCountries} center={mapCenter} zoom={mapZoom} />
+              countries={mapCountries} 
+              center={mapCenter} 
+              zoom={mapZoom} 
+            />
           </div>
         </div>
-        <Card className="stats-right">
+        <Card className="grow stats-right">
           <CardContent className="flexColumn flexBetween">
             <h3 className="live-cases">Live Count by Country</h3>
             <Table countries={table} />
